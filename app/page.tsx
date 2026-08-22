25d481440b67a0e74b75a563c4fecd56d60fe2b6
AlanAkmil <alansubagyo058@gmail.com>
2026-08-22 16:38:28 +0700

    add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import VideoGrid from "@/components/VideoGrid";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <VideoGrid />
+      <Footer />
+    </main>
+  );
+}
